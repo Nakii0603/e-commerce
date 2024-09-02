21042287da571b2d2a401ef25d62dfd5ff7906df
Nakii <guest1@Naakiizs-MacBook-Air.local>
2024-09-03 04:35:45 +0800

    fix

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <header className=" bg-slate-200">
-      <div className="flex mx-4 justify-between max-w-[1400px] mx-auto h-[50px] items-center">
+      <div className="flex  justify-between max-w-[1400px] mx-auto h-[50px] items-center">
         <h2 className="text-[30px]">Nova</h2>
         <div className="flex gap-10">
           <p>About</p>
