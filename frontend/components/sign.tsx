a2c9db3a34ef2d169224aa3d775fb2bd6851ecf7
Nakii <guest1@Naakiizs-MacBook-Air.local>
2024-09-04 22:10:41 +0800

    add stpper

--- frontend/components/sign.tsx
+++ frontend/components/sign.tsx
@@ -18,9 +18,18 @@ export default function SignIn() {
   };
 
   const handleNextStep = () => {
-    if (step === 1 && !formData.email) {
-      setErrorMessage("Email is required");
-      return;
+    if (step === 1) {
+      const emailInput = document.querySelector(
+        'input[type="email"]'
+      ) as HTMLInputElement;
+      if (!formData.email) {
+        setErrorMessage("Email is required");
+        return;
+      }
+      if (!emailInput.validity.valid) {
+        setErrorMessage("Invalid email format");
+        return;
+      }
     }
     if (step === 2 && (!formData.password || !formData.repassword)) {
       setErrorMessage("Both password fields are required");
@@ -55,7 +64,7 @@ export default function SignIn() {
         <h1 className="text-2xl flex justify-center mb-4">Бүртгүүлэх</h1>
 
         {step === 1 && (
-          <div>
+          <div className="flex flex-col gap-3">
             <input
               type="email"
               placeholder="Email"
@@ -63,6 +72,18 @@ export default function SignIn() {
               onChange={(e) => handleOnChange("email", e.target.value)}
               className="w-[300px] border-[1px] border-black h-[40px] rounded-md px-2"
             />
+            {errorMessage && (
+              <p className="text-red-500 " aria-live="assertive">
+                {errorMessage}
+              </p>
+            )}
+            <button
+              type="button"
+              onClick={handleNextStep}
+              className="w-[300px] h-[40px] bg-[#FF5349] text-white rounded-md"
+            >
+              Next
+            </button>
           </div>
         )}
 
@@ -82,44 +103,30 @@ export default function SignIn() {
               onChange={(e) => handleOnChange("repassword", e.target.value)}
               className="w-[300px] border-[1px] border-black h-[40px] rounded-md px-2"
             />
-          </div>
-        )}
-
-        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-
-        <div className="flex justify-between ">
-          {step > 1 && (
-            <div>
+            {errorMessage && (
+              <p className="text-red-500 text-center" aria-live="assertive">
+                {errorMessage}
+              </p>
+            )}
+            <div className="flex justify-between mt-4">
+              {step > 1 && (
+                <button
+                  type="button"
+                  onClick={handlePrevStep}
+                  className="w-[140px] h-[40px] bg-gray-300 text-black rounded-md"
+                >
+                  Back
+                </button>
+              )}
               <button
-                type="button"
-                onClick={handlePrevStep}
-                className="w-[150px] h-[40px] bg-gray-300 text-black rounded-md"
+                type="submit"
+                className="w-[140px] h-[40px] bg-[#FF5349] text-white rounded-md"
               >
-                Back
+                Submit
               </button>
             </div>
-          )}
-          {step < 2 ? (
-  
-
-          
-            <button
-              type="button"
-              onClick={handleNextStep}
-              className="w-[300px] h-[40px] bg-[#FF5349] text-white rounded-md"
-            >
-              Next
-            </button>
-    
-          ) : (
-            <button
-              type="submit"
-              className="w-[140px] h-[40px] bg-[#FF5349] text-white rounded-md"
-            >
-              Submit
-            </button>
-          )}
-        </div>
+          </div>
+        )}
       </form>
     </div>
   );
