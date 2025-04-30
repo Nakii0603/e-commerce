fd666e6808f97e331af000926fd7fd78575f2317
Nakii0603 <naakii0603@gmail.com>
2025-04-30 21:51:58 +0800

    fix admin dashboard

--- app/shop/components/ShopProfile.tsx
+++ app/shop/components/ShopProfile.tsx
@@ -1,31 +1,33 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import axios from "axios";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { tr } from "framer-motion/client";
 
 export default function ShopProfile() {
-  const [shop, setShop] = useState<string | null>(null);
-  const [shopData, setShopData] = useState<any>(null); // or a specific type if you know the structure
-  const [isModalOpen, setIsModalOpen] = useState(false); // To control modal visibility
+  const [shopData, setShopData] = useState<any>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
 
   const getInfo = async () => {
     try {
       const shopId = localStorage.getItem("shopId");
-      console.log(shopId);
-      const response = await axios.get(`${process.env.NEXT_PUBLIC_APP_API_URL}/api/auth/shop/getInfo/${shopId}`);
+      const token = localStorage.getItem("token");
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_APP_API_URL}/api/auth/shop/getInfo/${shopId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       const shopData = response.data.shop;
       setShopData(shopData);
-      setIsModalOpen(true)
+      setIsModalOpen(true);
     } catch (error) {
       console.log("Error during API call");
     }
   };
 
   const closeModal = () => {
-    setIsModalOpen(false); // Close the modal
+    setIsModalOpen(false);
   };
 
   return (
@@ -33,24 +35,25 @@ export default function ShopProfile() {
       <div className="w-[40px] text-center rounded-[4px] text-white  p-1 cursor-pointer" onClick={getInfo}>
         <img src="/user.png" alt="User Icon" />
       </div>
-
-      {/* Modal overlay with blurred background */}
       {isModalOpen && (
         <>
-          <div
-            className="fixed inset-0  bg-opacity-50 backdrop-blur-sm"
-            onClick={closeModal} // Close modal when clicking outside
-          ></div>
-
+          <div className="fixed inset-0  bg-opacity-50 backdrop-blur-sm" onClick={closeModal}></div>
           <div className="fixed right-0 top-0 w-[300px] h-full bg-white text-black border-[1px] border-black p-4 z-10 rounded-l-[8px]">
-            {" "}
             <h2 className="text-xl font-semibold">Shop Information</h2>
             {shopData && (
               <div>
-                <p>Shop Name: {shopData.shopName}</p>
-                <p>Email: {shopData.email}</p>
-                <p>Phone: {shopData.phoneNumber}</p>
-                <p>Shop ID: {shopData.shopId}</p>
+                <div className="flex">
+                  <p className=" font-bold">Shop Name:</p>
+                  <span>{shopData.shopName}</span>
+                </div>
+                <div className="flex">
+                  <p className=" font-bold">Email:</p>
+                  <span>{shopData.email}</span>
+                </div>
+                <div className="flex">
+                  <p className=" font-bold">Phone:</p>
+                  <span>{shopData.phoneNumber}</span>
+                </div>
               </div>
             )}
             <button onClick={closeModal} className="bg-red-500 text-white px-4 py-2 rounded mt-4">
