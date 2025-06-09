841e845482268f26082fb9b41fc1a87a7b73fb00
Nakii0603 <naakii0603@gmail.com>
2025-06-09 11:52:33 +0800

    a

--- app/user/dashboard/page.tsx
+++ app/user/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import DashboardProduct from '../components/dashbaord/DashboardProduct';
-import Navbar from '../components/auth/Navbar';
 
 export default function page() {
   return (
     <div >
-      <Navbar />
       <DashboardProduct />
     </div>
   );
